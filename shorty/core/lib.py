0c03b281e7d33e4993f229ed16c146529dfa70e6
Jacek Szpot <maligree@gmail.com>
2017-01-30 17:08:51 +0100

    Add missing docstrings in core.lib

--- shorty/core/lib.py
+++ shorty/core/lib.py
@@ -6,8 +6,14 @@ from django.conf import settings
 
 
 def get_random_user_from_db():
-    # Count first, then LIMIT + OFFSET so we
-    # don't actually pull the whole table into memory.
+    """
+    Returns a random User object from the database.
+
+
+    To prevent pulling the whole table into memory, we simply
+    count first, then query with LIMIT + OFFSET.
+    """
+
     user_count = User.objects.count()
     random_pos = randint(0, user_count - 1)
     random_user = User.objects.all()[random_pos:random_pos+1].get()
@@ -15,6 +21,13 @@ def get_random_user_from_db():
 
 
 def build_shortener_token(url):
+    """
+    Generate a [a-zA-Z0-9] token to represent the shortened URL.
+
+    The length of the token is random, based on the bounds specified
+    in SHORT_URL_LENGTH_BOUNDS in the app settings.
+    """
+
     min_len, max_len = settings.SHORT_URL_LENGTH_BOUNDS
     pool = string.ascii_uppercase + string.ascii_lowercase + string.digits
     return ''.join(choice(pool) for _ in range(randint(min_len, max_len)))
